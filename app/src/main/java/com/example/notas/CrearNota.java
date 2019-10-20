07772a4a47b6d6a732a9d7cfcb62e7265f82c4e8
danietb95 <danietb95@gmail.com>
2019-10-19 20:20:28 -0500

    Creación de boton en actividy de creación de nota

--- app/src/main/java/com/example/notas/CrearNota.java
+++ app/src/main/java/com/example/notas/CrearNota.java
@@ -2,7 +2,12 @@ package com.example.notas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class CrearNota extends AppCompatActivity {
@@ -23,8 +28,19 @@ public class CrearNota extends AppCompatActivity {
         getContext();
         TextView textViewTitle = (TextView) findViewById(R.id.titulo);
         TextView textViewDescription = (TextView) findViewById(R.id.descripcion);
+        Button botonGuardar = (Button)findViewById(R.id.guardar);
         textViewTitle.setText(titulo);
         textViewDescription.setText(descripcion);
+        botonGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView textViewTitle = (TextView) findViewById(R.id.titulo);
+                TextView textViewDescription = (TextView) findViewById(R.id.descripcion);
+                Intent intent = new Intent(CrearNota.this, MainActivity.class);
+                intent.putExtra("estado", "dd");
+                startActivity(intent);
+            }
+        });
 
     }
 }
